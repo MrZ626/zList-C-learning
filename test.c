1a526152db38a059eb386b4d1c0ea1cdf208dca8
MrZ626 <1046101471@qq.com>
2020-11-09 09:22:58 +0800

    更好的测试例

--- test.c
+++ test.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
-#include "linkedList.c"
+#include "linkedList.h"
+#define divLine printf("----------\n")
 int main()
 {
+	divLine;
+
+	printf("新建列表并在末尾添加五个数\n");
 	list L = list_new();
-	list_push(L, 3, 1);
-	list_push(L, 3, 2);
-	list_push(L, 3, 3);
-	list_push(L, 3, 4);
-	list_push(L, 3, 5);
-	for(int i=0;i<L->len;i++)
-		printf("%d\n", list_get(L,i));
+	list_push(L, -1, 1);
+	list_push(L, -1, 2);
+	list_push(L, -1, 3);
+	list_push(L, -1, 4);
+	list_push(L, -1, 5);
+	for (int i = 0; i < L->len; i++)
+		printf("%d\n", list_get(L, i));
+
+	divLine;
+
+	printf("移除第四个和第二个\n");
+	list_pop(L, 3);
+	list_pop(L, 1);
+	for (int i = 0; i < L->len; i++)
+		printf("%d\n", list_get(L, i));
+
+	divLine;
+
+	printf("向倒数第二和第四的位置插入两个数\n");
+	list_push(L, -2, 400);
+	list_push(L, -4, 200);
+	for (int i = 0; i < L->len; i++)
+		printf("%d\n", list_get(L, i));
+
+	divLine;
+
+	printf("释放链表\n");
+	list_free(L);
+	printf("完成\n");
+
+	divLine;
+
 	return 0;
 }
